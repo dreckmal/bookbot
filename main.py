d767d69603cb0025a280e5a95ba2ab2cce88c640
dreckmal <dreckmal@gmail.com>
2025-06-12 22:34:14 -0400

    added an alphabet counting function.

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ def get_book_text(path_to_file):
     return(file_contents)
 
 from stats import word_count
+from stats import alpha_count
 
 
 def main():
     
-    text_output = get_book_text("./books/frankenstein.txt")
-    print(text_output)
+#    text_output = get_book_text("./books/frankenstein.txt")
+#    print(text_output)
 
-    how_many_words = word_count("./books/frankenstein.txt")    
-    print(f'{how_many_words} words found in the document')
+#    how_many_words = word_count("./books/frankenstein.txt")    
+#    print(f'{how_many_words} words found in the document')
 
+    alphabet = alpha_count("./books/frankenstein.txt")
+    print(alphabet)
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,14 @@ def word_count(path_to_file):
         number_count = len(contents.split())
     return(number_count)
     
+frequency = {}
+def alpha_count(path_to_file):
+    with open(path_to_file) as w:
+        contents = w.read()
+        lower_case = contents.lower()
+        for alpha in lower_case:
+            if alpha in frequency:
+                frequency[alpha] += 1
+            else:
+                frequency[alpha] = 1
+    return(frequency)
